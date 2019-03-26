04e9bba051fd045fcb1eb7abe939207776795a8c
satwik <satwikraman5524@gmail.com>
2019-03-26 16:37:41 -0700

    26-03-2019

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,23 @@
 var User=require("../models/schema");
 var jwt = require('jsonwebtoken');
+var express=require('express');
+const mongoose=require("mongoose");
+const bodyParser=require("body-parser");
 var secret='harrypotter';
 var path=require("path");
-module.exports= (router)=> {
+const register=require('../models/schema');
+
+let dburl='mongodb://localhost:27017/Registrationdb'
+mongoose.connect(dburl, (err)=> {
+    if(err){
+        console.log("Not connected");
+    }else
+    {
+        console.log("succssfully connected");   //Database connection
+    }
+});
+
+const router=express.Router();
     router.post('/registration',(req,res)=>{
         register.name = req.body.name;
         register.password = req.body.password;
@@ -11,12 +26,9 @@ module.exports= (router)=> {
         if (req.body.name == null || req.body.name == '' || req.body.password == null || req.body.password == '' || req.body.email == null || req.body.phone == ''|| req.body.phone == null || req.body.phone == '') {
             res.json({success:false,message:'you have to give Username,password and email'});
         } else {
-            register.save(function (err) {
-                if (err) {
-                    throw err
-                } else {
-                    res.json({success:true,message:'User Created'});
-                }
+            register.create( (err)=> {
+                if (err) throw err
+                else res.json({success:true,message:'User Created'});
             });
         }
     
@@ -26,9 +38,7 @@ module.exports= (router)=> {
             if(err) throw err;
     
             if(!register) {
-    
                 res.send( 'Could not Authenticate User');
-    
             }
             else if(register){
                 if(req.body.password){
@@ -50,4 +60,4 @@ module.exports= (router)=> {
     
     });   
     });
-}
+module.exports=router;
--- server.js
+++ server.js
@@ -3,28 +3,15 @@ const app=express();
 const morgan=require("morgan");
 const port=process.env.PORT || 4000;
 const mongoose=require("mongoose");
-// var User=require("./appss/models/user");      // packages
 const bodyParser=require("body-parser");
 const bcrypt=require("bcrypt-nodejs");
-const router=express.Router();
 const passport = require('passport');
 
-var appRoutes= require('./routes/api')(router);
+var appRoutes= require('./routes/api');
 var path=require("path");
 
 
-const Register=require('./models/schema');
-
-let dburl='mongodb://localhost:27017/Registrationdb'
-mongoose.connect(dburl, (err)=> {
-    if(err){
-        console.log("Not connected");
-    }else
-    {
-        console.log("succssfully connected");   //Database connection
-    }
-});
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/api',appRoutes);
